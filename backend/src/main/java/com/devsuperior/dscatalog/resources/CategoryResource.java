0c204ca944c6b5794b880be3caf8427dd79a8525
italosilva98 <italosilvestre3@icloud.com>
2021-10-23 18:53:12 -0300

    Add POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,17 +1,20 @@
 package com.devsuperior.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 @RestController // indica que essa classe sera um controlador REST
 @RequestMapping(value = "/categories") // define a rota
@@ -37,11 +40,21 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
 		// @PathVariable utilizado para casar o parametro com o id na rota
 
-			CategoryDTO dto = service.findById(id);
-			
-		
+		CategoryDTO dto = service.findById(id);
 
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		// @RequestBody utilizado para informar q vai no body
+
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+
 	}
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/EntityNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -6,6 +6,4 @@ public class EntityNotFoundException extends RuntimeException {
 	public EntityNotFoundException(String msg) {
 		super(msg);
 	}
-	
-	
 }
