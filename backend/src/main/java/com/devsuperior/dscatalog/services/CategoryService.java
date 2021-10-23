7db07be1ee0be20f3e69182c2fae618cbaaefaac
italosilva98 <italosilvestre3@icloud.com>
2021-10-23 18:02:43 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired // para fazer a injeção de dependência automaticamente
 	private CategoryRepository repository;
 
+	@Transactional(readOnly = true) // garantir a integridade e o readonly, usa sempre quando for uma busca
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
